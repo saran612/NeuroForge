831e54ba9ef78dbecea78e1e9aa8a654a7ae54ac
saran612 <freelancexsaran@gmail.com>
2025-07-29 09:09:42 +0530

    Initial commit

---
+++ src/components/Menubar.tsx
@@ -0,0 +1,47 @@
+import './Menubar.css'
+
+function Menubar(){
+    return (
+    <>
+        <section className="menu">
+            <div className="box-1">
+                NEUROFROGE
+            </div>
+            <div className="box-2">
+                <div className="list">
+                    <div className="menu_items">
+                        <div>
+                            <a href="" target="_self" >Home</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Problem</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self">Solution</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Research</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Prototype</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Impact</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Team</a>
+                        </div>
+                        <div>
+                            <a href="" target="_self" >Contact</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="circle">
+                </div>
+            </div>
+        </section>
+    </>
+);
+}
+
+export default Menubar;
