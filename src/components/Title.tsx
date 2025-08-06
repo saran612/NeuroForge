525db717698ef4f294c4504374bb6d7a6d0c4135
saran612 <freelancexsaran@gmail.com>
2025-08-06 14:32:25 +0530

    update Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,7 @@ function Title(){
         <>
             <section className='Title'>
                 <div className='Title_body'>MEMORA</div>
-                <img src='https://github.com/saran612/NeuroForge/blob/main/public/Group%204.svg' alt='Build for NCIIPC' className='Img_Title'></img>
+                <img src='https://raw.githubusercontent.com/saran612/NeuroForge/f873cd2cff17e347c4b41dade4fa9c11ad257eb2/public/Group%204.svg' alt='Build for NCIIPC' className='Img_Title'></img>
             </section> 
         </>
     )
