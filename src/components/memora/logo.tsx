0562589c6aa879e7a09b7ee4f85f30b1640e796f
saran612 <freelancexsaran@gmail.com>
2025-08-09 11:47:05 +0530

    Remove all files

--- src/components/memora/logo.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/lib/utils';
-import React from 'react';
-
-export const MemoraLogo = ({ className }: { className?: string }) => (
-    <div className={cn('font-headline font-black text-8xl md:text-9xl text-primary-foreground', className)} style={{ fontFamily: 'Poppins, sans-serif' }}>
-        MEMORA
-    </div>
-);
